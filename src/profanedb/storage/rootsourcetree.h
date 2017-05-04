60b6f8367a8d674ff85f62dee575204e2d3c158d
Giorgio Azzinnaro <giorgio.azzinnaro@gmail.com>
2017-05-04 22:38:06 +0200

    fix in project name in headers

--- src/profanedb/storage/rootsourcetree.h
+++ src/profanedb/storage/rootsourcetree.h
@@ -1,5 +1,5 @@
 /*
- * ProfaneDB - A Protocol Buffer database.
+ * ProfaneDB - A Protocol Buffers database.
  * Copyright (C) 2017  "Giorgio Azzinnaro" <giorgio.azzinnaro@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
